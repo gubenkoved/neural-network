3e7f9affe83a03502383afcfa7c1e92e13883c3d
Gubenkov Eugene <gubenkoved@gmail.com>
2016-05-29 23:48:20 +0300

    Added Test Console.

---
+++ NeuroNet.TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using NeuroNet.Core.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroNet.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nn = NeuronetWithInformation.Load(@"D:\Repos\NeuroNet\NeuroNet.Web\Neuronets\16x16.9000.150");
+        }
+    }
+}
